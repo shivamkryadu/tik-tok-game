c4787d9765400cb5e20d2cd2df400d29e72c8907
Shivam Kumar <162969182+shivamkryadu@users.noreply.github.com>
2024-04-02 09:14:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetbtn");
+let newGameBtn = document.querySelector(".new");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turno = true;
+
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+const restGame = () => {
+    turno = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turno) {
+            box.innerText = "O";
+            turno = false;
+        }
+        else {
+            box.innerText = "X"
+            turno = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerHTML = "";
+    }
+}
+const showWinner = (winner) => {
+    msg.innerText = 'Congratulation,You Win the Game';
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for (let pattern of winPattern) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val == pos2val && pos2val == pos3val) {
+                console.log("winner", pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", restGame);
+resetBtn.addEventListener("click", restGame);
